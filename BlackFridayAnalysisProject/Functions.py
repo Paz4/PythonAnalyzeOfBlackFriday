1410d6ac3ba236338bcf81ebfefd8c493cf11d21
Paz4 <noizemi@gmail.com>
2018-12-24 02:28:56 +0700

    Graphics was improved, names was reffactored
    Diagrames become more informative. Names was changed on more appropriate.
    Spotted diagramm was added

--- BlackFridayAnalysisProject/Functions.py
+++ BlackFridayAnalysisProject/Functions.py
@@ -11,5 +11,16 @@ def draw_pie_diagram(name):
     print(name)
     name.plot(kind='pie')
     plt.axis('equal')
-    plt.show()    
+    plt.show()
 
+def draw_pie_diagram_advanced(name, lst=[], exp=None):
+    print(name)
+    if exp != None:
+           exp = exp
+    fig1, ax1 = plt.subplots(figsize=(12,7))
+    ax1.pie(name, explode=exp, labels=lst, autopct='%1.1f%%', shadow=True, startangle=90)
+    # Equal aspect ratio ensures that pie is drawn as a circle
+    ax1.axis('equal')  
+    plt.tight_layout()
+    plt.legend()
+    plt.show()
